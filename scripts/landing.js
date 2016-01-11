d6c0206db0ba76c3afeb562025bebcae70d0d224
Nicole Wade <Nicole.Wade@target.com>
2016-01-10 19:02:10 -0600

    cp23 added scrolling for points

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,7 @@
-var animatePoints = function() {
-     var revealPoint = function() {
-         var points = document.getElementsByClassName('point');
+var points = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+     var revealPoint = function(points) {
          
          for(var i = 0; i < points.length; i++){
          points[i].style.opacity = 1;
@@ -9,6 +10,20 @@ var animatePoints = function() {
          points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
          };
      };
-revealPoint();
+revealPoint(points);
  };
-animatePoints();
+
+window.onload = function(){
+         if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event){
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(points);   
+         }
+    });
+    //animatePoints();
+}
